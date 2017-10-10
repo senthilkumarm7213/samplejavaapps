91bdfcfbbe5024b1d4fabcb6c87bd66e5e4218ea
senthilkumarm7213 <senthilkumar.m7213@gmail.com>
2017-10-10 08:20:28 +0530

    Initial commit

---
+++ src/main/java/samplejavaapps/samplejavaapps/App.java
@@ -0,0 +1,13 @@
+package samplejavaapps.samplejavaapps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
